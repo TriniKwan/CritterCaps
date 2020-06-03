7c3c206eaca78f6f6ba47310523180785d446114
Monica Djunaidi <modjun12@gmail.com>
2020-06-02 20:51:55 -0500

    wip

--- CritterCaps/Controllers/CritterCapsController.cs
+++ CritterCaps/Controllers/CritterCapsController.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using CritterCaps.DataAccess;
 
 namespace CritterCaps.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/critterCaps")]
     [ApiController]
     public class CritterCapsController : ControllerBase
     {
+        CritterCapsRepository _repository = new CritterCapsRepository();
+
+        [HttpGet("productType")]
+        public IActionResult GetAllProductTypes()
+        {
+            var result = _repository.GetAllProductTypes();
+            return Ok(result);
+        }
     }
 }
---
+++ CritterCaps/DataAccess/CritterCapsRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using CritterCaps.Models;
+
+
+namespace CritterCaps.DataAccess
+{
+    public class CritterCapsRepository
+    {
+        string ConnectionString = "Server = localhost; Database = CandyMarket; Trusted_Connection = True;";
+        public List<ProductType> GetAllProductTypes()
+        {
+            var sql = @"select *
+                        from ProductType";
+
+            using (var db = new SqlConnection(ConnectionString))
+        }
+    }
+}
