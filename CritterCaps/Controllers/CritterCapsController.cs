ef0a4568b74ad854c29b1c82072a86cdc178a60d
Monica Djunaidi <modjun12@gmail.com>
2020-06-03 14:33:56 -0500

    added reconfiguration, can get all product types, can get single product type

--- CritterCaps/Controllers/CritterCapsController.cs
+++ CritterCaps/Controllers/CritterCapsController.cs
@@ -12,12 +12,28 @@ namespace CritterCaps.Controllers
     [ApiController]
     public class CritterCapsController : ControllerBase
     {
-        CritterCapsRepository _repository = new CritterCapsRepository();
+        CritterCapsRepository _repository;
 
-        [HttpGet("productType")]
+        public CritterCapsController(CritterCapsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("productTypes")]
         public IActionResult GetAllProductTypes()
         {
             var result = _repository.GetAllProductTypes();
+            return Ok(result);
+        }
+
+        [HttpGet("productType/{productType}")]
+        public IActionResult GetSingleProductType(string productType)
+        {
+            var result = _repository.GetSingleProductType(productType);
+            if (result == null)
+            {
+                return NotFound("Oops! We haven't had time to craft that type of hat.");
+            }
             return Ok(result);
         }
     }
--- CritterCaps/DataAccess/CritterCapsRepository.cs
+++ CritterCaps/DataAccess/CritterCapsRepository.cs
@@ -5,13 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
 using Dapper;
 using CritterCaps.Models;
+using Microsoft.Extensions.Configuration;
 
 
 namespace CritterCaps.DataAccess
 {
     public class CritterCapsRepository
     {
-        string ConnectionString = "Server = localhost; Database = CandyMarket; Trusted_Connection = True;";
+        string ConnectionString;
+
+        public CritterCapsRepository(IConfiguration config)
+        {
+            ConnectionString = config.GetConnectionString("CritterCaps");
+        }
         public List<ProductType> GetAllProductTypes()
         {
             var sql = @"select *
@@ -21,6 +27,19 @@ namespace CritterCaps.DataAccess
             {
                 var productTypes = db.Query<ProductType>(sql).ToList();
                 return productTypes;
+            }
+        }
+
+        public ProductType GetSingleProductType(string productType)
+        {
+            var sql = @"select *
+                        from ProductType
+                        where Category = @productType";
+
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var singleProductType = db.QueryFirstOrDefault<ProductType>(sql, new { ProductType = productType });
+                return singleProductType;
             }
         }
     }
