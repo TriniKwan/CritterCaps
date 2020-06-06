01a63abf9cdc6ec36c1871cf2116c5949fd7090b
Laura Collins <laura.e.collins1@gmail.com>
2020-06-06 09:11:37 -0500

    can get all orders

---
+++ CritterCaps/Repositories/OrdersRepository.cs
@@ -0,0 +1,39 @@
+﻿using CritterCaps.Models;
+using System;
+using System.Collections.Generic;
+using Dapper;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace CritterCaps.Repositories
+{
+    public class OrdersRepository
+    {
+        string ConnectionString;
+
+        public OrdersRepository(IConfiguration config)
+        {
+            ConnectionString = config.GetConnectionString("CritterCaps");
+        }
+
+        public IEnumerable<Orders> GetAllOrders()
+        {
+            var sql = $@"SELECT [Order].OrderId, [User].FirstName + ' ' + [User].LastName AS CustomerName, [Order].InvoiceDate, [Order].Total, PaymentType.[Type] AS PaymentType
+                         FROM[Order]
+                            JOIN[User]
+                            ON[Order].UserId = [User].ID
+                            JOIN PaymentType
+                            ON PaymentType.PaymentID = [Order].PaymentType";
+
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var orders = db.Query<Orders>(sql);
+
+                return orders;
+            }
+        }
+
+    }
+}
