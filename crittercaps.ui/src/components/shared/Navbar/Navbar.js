c7c3829d6cfb680e7666915f03494e4792c0e76f
Randy Tate <randytatejr@gmail.com>
2020-06-15 20:38:10 -0500

    setup navbar

--- crittercaps.ui/src/components/shared/Navbar/Navbar.js
+++ crittercaps.ui/src/components/shared/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../Navbar/Navbar.scss';
 
 class Navbar extends React.Component {
@@ -6,29 +7,28 @@ class Navbar extends React.Component {
         return (
             <div className="Navbar">
                 <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-                <a className="navbar-brand" to="/home">
+                <Link className="navbar-brand" to="/">
                 <img src="https://user-images.githubusercontent.com/51382883/84576012-1bf55d80-ad77-11ea-8fb4-b440ed0972b0.png" className="logo-image" alt="CritterCaps Logo" />
-                </a>
-                <h3 className="appTitle">CritterCaps!</h3>
+                </Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
+                    <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                        <a className="nav-link" id="navvy-link" to="/home">Home</a>
+                        <Link className="nav-link" id="navvy-link" to="/">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" id="navvy-link" to="/teams">Hats</a>
+                        <Link className="nav-link" id="navvy-link" to="/products">Hats</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" id="navvy-link" to="/waitlist">Profile</a>
+                        <Link className="nav-link" id="navvy-link" to="/userProfile">Profile</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" id="navvy-link" to="/availableplayers">Shopping Cart</a>
+                        <Link className="nav-link" id="navvy-link" to="/userProfile/shoppingCart">Shopping Cart</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" id="navvy-link" to="/availableplayers">Register/LogIn</a>
+                        <Link className="nav-link" id="navvy-link" to="/register">Register/LogIn</Link>
                     </li>
                     </ul>
                 </div>
