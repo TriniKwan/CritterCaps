95fadd4b2306ce2bd8d2569798a1ccfcc1e00982
Randy Tate <randytatejr@gmail.com>
2020-06-17 11:20:18 -0500

    data loaded to page

--- crittercaps.ui/src/components/pages/Products/Products.js
+++ crittercaps.ui/src/components/pages/Products/Products.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './Products.scss';
 import { Link } from 'react-router-dom';
+import ProductCard from '../../shared/ProductCard/ProductCard';
+import ProductData from '../../../helpers/data/ProductData';
 
 class Products extends React.Component {
   state = {
-    productId: 1,
+    // productId: 1,
+    products: [],
   }
 
+  componentDidMount() {
+    ProductData.getAllAvailableProducts()
+      .then((products) => this.setState({ products }))
+      .catch((error) => console.error(error, 'error from allProducts'));
+  }
+  
   render() {
-    const { productId } = this.state;
+    // const { productId } = this.state;
+    const { products } = this.state;
 
     return (
       <div className="Products">
         <h1>Products</h1>
-        <Link to={`/products/${productId}`} className="btn btn-primary">Single Product</Link>
+        { products.map((product) => <ProductCard key={product.productId} product={product} />) };
+        {/* <Link to={`/products/${productId}`} className="btn btn-primary">Single Product</Link> */}
       </div>
     );
   }
