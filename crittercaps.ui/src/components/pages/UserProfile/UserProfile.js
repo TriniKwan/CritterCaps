7e153704f697f6bf7985c311f3cea3ccd7ecb450
Randy Tate <randytatejr@gmail.com>
2020-06-17 18:21:10 -0500

    WIP

--- crittercaps.ui/src/components/pages/UserProfile/UserProfile.js
+++ crittercaps.ui/src/components/pages/UserProfile/UserProfile.js
@@ -3,15 +3,29 @@ import './UserProfile.scss';
 import { Link } from 'react-router-dom';
 
 class UserProfile extends React.Component {
+  state = {
+    users: [],
+  }
+
   render() {
+    const { users } = this.state;
+
     return (
       <div className="UserProfile">
         <h1>User Profile</h1>
         <Link to="/userProfile/orders" className="btn btn-primary">Orders</Link>
         <Link to="/userProfile/shoppingCart" className="btn btn-danger" >Shopping Cart</Link>
+        {/* { product.map((product) => <ProductCard key={product.productId} product={product} />) } */}
       </div>
     );
   }
 }
 
 export default UserProfile;
+
+
+// componentDidMount() {
+//   productData.getNewestProducts()
+//     .then((products) => this.setState({ products }))
+//     .catch((error) => console.error(error, 'error from products'));
+// }
---
+++ crittercaps.ui/src/components/shared/UserProfileCard/UserProfileCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class UserProfileCard extends React.Component {
+    state = {
+        users: [],
+    }
+    // id, firstName, lastName, accountDate, administrator
+    render() {
+        return(
+            <div className="UserProfileCard">
+
+            </div>
+        );
+    }
+}
+
+export default UserProfileCard;
