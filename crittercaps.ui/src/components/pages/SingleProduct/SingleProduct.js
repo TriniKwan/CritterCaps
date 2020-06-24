685fc50301d2cd21074637abdffd62d6d4651e58
Laura Collins <laura.e.collins1@gmail.com>
2020-06-23 21:14:24 -0500

    can detect user and orders

--- crittercaps.ui/src/components/pages/SingleProduct/SingleProduct.js
+++ crittercaps.ui/src/components/pages/SingleProduct/SingleProduct.js
@@ -2,12 +2,75 @@ import React from 'react';
 import './SingleProduct.scss';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import firebase from 'firebase/app';
 import ProductData from '../../../helpers/data/ProductData';
+import orderData from '../../../helpers/data/orderData';
+import 'firebase/auth';
+import authData from '../../../helpers/data/authData';
 
 class SingleProduct extends React.Component {
   state = {
     product: {},
+    userId: '',
+    orderId: '',
+    cartExists: false,
   }
+
+  getUser = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        if (sessionStorage.getItem('token')) {
+          const uid = authData.getUid();
+          authData.getUserByUid(uid)
+            .then((userData) => {
+              if (userData) {
+                this.setState({ userId: userData.id });
+                this.getOrder(this.state.userId);
+              }
+            })
+            .catch((error) => console.error('error from getUser', error));
+        }
+      } else {
+        this.setState({ userId: '', cartExists: false, orderId: '' });
+      }
+    });
+  }
+
+  getOrder = (userId) => {
+    orderData.getOpenOrder(userId)
+      .then((cart) => {
+        if (cart !== 'No order exists') {
+          console.error(cart);
+          this.setState({ cartExists: true });
+          this.setState({ orderId: cart.orderId });
+        }
+      })
+      .catch((error) => {
+        if (error === 'No order exists') {
+          this.setState({ cartExists: false, orderId: '' });
+        }
+      });
+  }
+
+  checkExistingOrderAndCreateNew = () => {
+    const { userId } = this.state;
+    if (userId === '') {
+      authData.loginUser();
+    }
+
+    // orderData.getOpenOrder(userId)
+    //   .then((response) => {
+    //     console.log(response);
+    //   })
+    //   .catch((error) => console.error(error));
+    // if (order.StatusCode === 404) {
+    //   orderData.createNewOrder(userId)
+    //     .then((result) => console.log(result))
+    //     .catch((error) => console.error('err from check and create', error));
+    // }
+    // return order;
+  };
 
   componentDidMount() {
     const { productId } = this.props.match.params;
@@ -15,6 +78,7 @@ class SingleProduct extends React.Component {
     ProductData.getSingleProduct(productId)
       .then((product) => this.setState({ product }))
       .catch((error) => console.error(error, 'error from single product'));
+    this.getUser();
   }
 
   render() {
@@ -40,7 +104,7 @@ class SingleProduct extends React.Component {
                 Price: {price}
               </Card.Text>
               <Link to="/" className="btn btn-info m-2">Back</Link>
-              <Link to={`/products/${product}`} className="btn btn-info m-2">Add To Cart</Link>
+              <Button onClick={this.checkExistingOrderAndCreateNew} className="btn btn-info m-2">Add To Cart</Button>
             </Card.Footer>
           </Card>
       </div>
--- crittercaps.ui/src/helpers/data/orderData.js
+++ crittercaps.ui/src/helpers/data/orderData.js
@@ -3,8 +3,22 @@ import apiKeys from '../apiKeys.json';
 
 const { baseUrl } = apiKeys;
 
-const getOpenOrder = (userId) => new Promise((resolve, reject) => {
+const getOpenOrder = (userId) => new Promise((resolve) => {
   axios.get(`${baseUrl}/api/crittercaps/orders/shoppingCart/${userId}`)
+    .then((result) => {
+      const openOrder = result.data;
+      resolve(openOrder);
+    })
+    .catch((error) => {
+      if (error.StatusCode === 404) {
+        return ('No order exists');
+      }
+      return (error);
+    });
+});
+
+const addItem = (orderId, productId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/api/crittercaps/orders/addItem/orderId/${orderId}/productId/${productId}`)
     .then((result) => {
       const openOrder = result.data;
       resolve(openOrder);
@@ -12,4 +26,17 @@ const getOpenOrder = (userId) => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
-export default { getOpenOrder };
+const createNewOrder = (userId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/api/crittercaps/orders/order/new/userId/${userId}`)
+    .then((result) => {
+      const openOrder = result.data;
+      resolve(openOrder);
+    })
+    .catch((error) => reject(error));
+});
+
+export default {
+  getOpenOrder,
+  addItem,
+  createNewOrder,
+};
