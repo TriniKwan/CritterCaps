d83453d599583ea247364f8026250c6bf8116f13
Monica Djunaidi <modjun12@gmail.com>
2020-06-27 10:34:38 -0500

    updated price to display dollar currency

--- crittercaps.ui/src/components/shared/OrderCard/OrderCard.js
+++ crittercaps.ui/src/components/shared/OrderCard/OrderCard.js
@@ -4,12 +4,13 @@ import Card from 'react-bootstrap/Card';
 class OrderCard extends React.Component {
   render() {
     const { customerName, invoiceDate, total } = this.props.order;
+    const price = Number(total).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 
     return (
       <Card style={{ width: '18rem' }} className="h-100 d-flex justify-content-center" border="primary" >
         <Card.Title>Customer Name: { customerName }</Card.Title>
         <Card.Text>Date of Purchase: { invoiceDate }</Card.Text>
-        <Card.Text>Total: { total }</Card.Text>
+        <Card.Text>Total: { price }</Card.Text>
       </Card>
     );
   }
